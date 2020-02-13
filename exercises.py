7025355e945ee7d8da0e03d468ee81f8edc54802
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-02-13 03:12:52 +0000

    started only one test pass

--- exercises.py
+++ exercises.py
@@ -1,28 +1,44 @@
 """
 Recursion Problem Set: Coding Exercises
 """
+from functools import lru_cache
 
 def reverse(s: str) -> str:
-    """
-    Reverse string using recursive approach
-    """
+    if len(s) <= 1:
+        return s
+    else:
+        return reverse(s[1:])  + s[0]
 
 def fib_rec(n: int) -> int:
-    """
-    Recursive fibonacci
-    """
-
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return fib_rec(n - 1) + fib_rec(n - 1)
+    
+    
+cache = {}
 def fib_dyn(n: int) -> int:
-    """
-    Fibonacci using dynamic programming (w/ a cache)
-    """
+    if n == 1 or n == 0:
+        return 1
+    
+    if n in cache:
+        return cache[n]
+    else:
+        cache[n] = fib_dyn(n - 1) + fib_dyn(n - 2)
+        return cache[n]
 
 def fib_iter(n: int) -> int:
-    """
-    Iterative approach to solving the fibonacci problem
-    """
+    last_number = 1
+    two_before = 1
+    if n == 1 or n == 0:
+        return 1
+    
+    for i in range(2,n + 1):
+        n = last_number + two_before
+        two_before = last_number
+        last_number = n 
+    return n
 
 def to_str(n: int, base: int) -> int:
-    """
-    convert integer to a string of a given base
-    """
+    #Base Case
+    pass
