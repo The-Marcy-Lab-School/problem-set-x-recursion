4e7cc0c8b466ce8a8f116e80087cfa0418ef289b
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2020-02-14 19:06:51 +0000

    Short Response Complete

--- exercises.py
+++ exercises.py
@@ -3,9 +3,27 @@ Recursion Problem Set: Coding Exercises
 """
 
 def reverse(s: str) -> str:
-    """
-    Reverse string using recursive approach
-    """
+#Reverse string using recursive approach
+
+# Short-hand PEDAC.
+# Note to self: Dont make this a habit
+
+
+# 1. Functions takes a string
+
+# 2. Put the base condition that if the length of the string is equal to 0 or 1, the string is returned.
+#    You cant reverse a string with 1 or no characters. Has to be a first and last character.
+
+# 3. Recursively call the reverse function to slice the part of the string except the first character 
+and concatenate the first character to the end of the sliced string.
+5. Print the reversed string.
+6. Exit.
+
+if len(string) == 0:
+        return string
+    else:
+        pass        
+        
 
 def fib_rec(n: int) -> int:
     """
