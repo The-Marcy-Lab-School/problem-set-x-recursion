d0e6f43cdf9450ffd70624b2ebf6d5877f6e7c5c
EC2 Default User <ec2-user@ip-172-31-24-236.us-east-2.compute.internal>
2020-02-18 04:11:16 +0000

    added answers

--- exercises.py
+++ exercises.py
@@ -2,27 +2,35 @@
 Recursion Problem Set: Coding Exercises
 """
 
-def reverse(s: str) -> str:
-    """
-    Reverse string using recursive approach
-    """
+def reverse(string):
+    if  len(string) == 1:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+        
+def fib_rec(num: int) -> int:
+    if num == 0 or num == 1 or num == 2:
+        return 1
+    else:
+        return fib_rec(num - 2) + fib_rec(num - 1)
 
-def fib_rec(n: int) -> int:
-    """
-    Recursive fibonacci
-    """
 
 def fib_dyn(n: int) -> int:
-    """
-    Fibonacci using dynamic programming (w/ a cache)
-    """
+    pass
 
-def fib_iter(n: int) -> int:
-    """
-    Iterative approach to solving the fibonacci problem
-    """
+def fib_iter(num: int) -> int:
+    first = 0
+    second = 1
+    
+    for i in range(num):
+        nth = first + second
+        first = second
+        second = nth
+    return first
 
-def to_str(n: int, base: int) -> int:
-    """
-    convert integer to a string of a given base
-    """
+def to_str(num: int, base: int) -> int:
+    convert = "0123456789ABCDEF"
+    if num < base:
+        return convert[num]
+    else:
+        return to_str(num//base, base) + convert[num%base]
