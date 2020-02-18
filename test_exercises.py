9cbdc40e94dbd668cee1cde95a7543ddb9fe754a
Enmanuel <54204155+emtes@users.noreply.github.com>
2020-02-17 23:27:29 -0500

    fix fib tests

--- test_exercises.py
+++ test_exercises.py
@@ -9,23 +9,23 @@ def test_reverse():
 
 @pytest.mark.skip()
 def test_fib_rec():
-    assert fib_rec(10) == 5
+    assert fib_rec(10) == 55
     assert fib_rec(1) == 1
-    assert fib_rec(0) == 1
+    assert fib_rec(0) == 0
     assert fib_rec(20) == 6765
 
 @pytest.mark.skip()
 def test_fib_dyn():
-    assert fib_dyn(10) == 5
+    assert fib_dyn(10) == 55
     assert fib_dyn(1) == 1
-    assert fib_dyn(0) == 1
+    assert fib_dyn(0) == 0
     assert fib_dyn(20) == 6765
 
 @pytest.mark.skip()
 def test_fib_iter():
-    assert fib_iter(10) == 5
+    assert fib_iter(10) == 55
     assert fib_iter(1) == 1
-    assert fib_iter(0) == 1
+    assert fib_iter(0) == 0
     assert fib_iter(20) == 6765
 
 @pytest.mark.skip()
