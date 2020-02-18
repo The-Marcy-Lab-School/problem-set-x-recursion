a76473235236c2901527901690e283f629f38e87
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-02-18 04:38:41 +0000

    add solutions

--- exercises.py
+++ exercises.py
@@ -1,28 +1,40 @@
+from typing import Dict
+
 """
 Recursion Problem Set: Coding Exercises
 """
 
 def reverse(s: str) -> str:
-    """
-    Reverse string using recursive approach
-    """
+	if len(s) == 1:
+		return s
+	else:
+		return reverse(s[1:]) + reverse(s[0])
 
 def fib_rec(n: int) -> int:
-    """
-    Recursive fibonacci
-    """
+	if n < 2:
+		return n
+	else:
+		return fib_rec(n - 2) + fib_rec(n - 1)
 
 def fib_dyn(n: int) -> int:
-    """
-    Fibonacci using dynamic programming (w/ a cache)
-    """
+	cache: Dict[int, int] = {0: 0, 1: 1}
+	if n not in cache:
+		cache[n] = fib_dyn(n - 2) + fib_dyn(n - 1)
+	return cache[n]
 
 def fib_iter(n: int) -> int:
-    """
-    Iterative approach to solving the fibonacci problem
-    """
+	if n < 2: return n
+	two_before: int = 0
+	one_before: int = 1
+	for _ in range(1, n):
+		n = one_before + two_before
+		two_before = one_before
+		one_before = n
+	return n
 
-def to_str(n: int, base: int) -> int:
-    """
-    convert integer to a string of a given base
-    """
+def to_str(n: int, base: int) -> str:
+	conversion_chars: str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	if n < base:
+		return conversion_chars[n]
+	else:
+		return to_str(n // base, base) + conversion_chars[n % base]
