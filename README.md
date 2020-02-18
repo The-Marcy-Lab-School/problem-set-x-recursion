d9fc7fa93467501acbb053318380502887226fa7
EC2 Default User <ec2-user@ip-172-31-16-222.us-east-2.compute.internal>
2020-02-18 06:51:11 +0000

    Completed kinda

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ Respond to short response questions in clear, concise sentences directly within 
 ### Short Response Questions
 **1. What is recursion?**
 
+    Recursion is essentially the process through which something is done over and over. An example of this is recursive functions that actually invoke themselves within themselves with the output of the previous iteration.
+
 **2. What type of problems are best solved using recursion?**
+
+    the problems best solved with recursion are ones that need to be completed over multiple iterations but lose the advantage of readability when formatted into a loop.
 
 **3. What are the benefits of writing functions recursively? What are the drawbacks?**
 
+    One of the benefits of using recursive functions is readability, it allows a programmer to write code that can be read intuitively rather than a loop that is less transparent in is operations. One of the drawbacks however is that with each nested invocation of a recursive function, a function is added to the call stack, which for functions with larger inputs/outputs would be disadvantgeous.
 ### Coding Exercises
 Answer the following questions in `exercises.py`. Run unit test with the `pytest` command. Ensure all tests are passing before submitting this problem set.
 
--- exercises.py
+++ exercises.py
@@ -3,26 +3,55 @@ Recursion Problem Set: Coding Exercises
 """
 
 def reverse(s: str) -> str:
-    """
-    Reverse string using recursive approach
-    """
+    if len(s) <= 1:
+        return s
+    return reverse(s[1:]) + s[0]
 
 def fib_rec(n: int) -> int:
-    """
-    Recursive fibonacci
-    """
+    if n == 0 or n == 1:
+        return 1
+    return fib_rec(n - 1) + fib_rec(n-2)
+
+
+def memoize(f):
+    cache = {}
+    def helper(num):
+        if num not in cache:            
+            cache[num] = f(num)
+        return cache[num]
+    return helper
+    
 
 def fib_dyn(n: int) -> int:
-    """
-    Fibonacci using dynamic programming (w/ a cache)
-    """
+    if n == 0 or n == 1:
+        return 1
+    return fib_rec(n - 1) + fib_rec(n-2)
+
+fib_dyn = memoize(fib_dyn)
 
 def fib_iter(n: int) -> int:
-    """
-    Iterative approach to solving the fibonacci problem
-    """
+   old = 0
+   new = 1
+   count = 0
+   while count < n:
+       old_new = new 
+       new = old + new
+       old = old_new
+       count += 1
+   return new
+       
 
+"""
+Below is an MVP, should be refactored to be recursive soon
+"""
 def to_str(n: int, base: int) -> int:
-    """
-    convert integer to a string of a given base
-    """
+    new_int = ""
+    
+    if base == 10:
+        return str(n)
+        
+    while n > 1:
+        new_int = str(int(n % base)) + new_int
+        n = n/base
+    return new_int
+    
