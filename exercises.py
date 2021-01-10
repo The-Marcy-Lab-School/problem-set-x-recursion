190d4ffdede3d0b6c88765005519e838f3612824
Ann Duong <thuyanannduong@gmail.com>
2021-01-10 13:03:34 -0500

    fixed tests

--- exercises.py
+++ exercises.py
@@ -7,21 +7,16 @@ def reverse(s: str) -> str:
     Reverse string using recursive approach
     """
 
-def fib_rec(n: int) -> int:
-    """
-    Recursive fibonacci
-    """
-
-def fib_dyn(n: int) -> int:
-    """
-    Fibonacci using dynamic programming (w/ a cache)
-    """
-
 def fib_iter(n: int) -> int:
     """
     Iterative approach to solving the fibonacci problem
     """
 
+def fib_rec(n: int) -> int:
+    """
+    Recursive fibonacci
+    """
+    
 def to_str(n: int, base: int) -> str:
     """
     convert integer to a string of a given base
--- exercises.test.js
+++ exercises.test.js
@@ -1,6 +1,6 @@
 const { reverse, fibRec, fibDyn, fibIter, toStr } = require('./exercises.js');
 
-describe.skip('reverse', () => {
+describe('reverse', () => {
   it('can reverse a simple string', () => {
     expect(reverse('hello')).toEqual('olleh');
   });
@@ -14,36 +14,27 @@ describe.skip('reverse', () => {
   });
 });
 
-describe.skip('fibRec', () => {
-  it('given place, retursn the Fibonacci number', () => {
-     expect(fibRec(10)).toEqual(55);
-      expect(fibRec(1)).toEqual(1);
-      expect(fibRec(0)).toEqual(0);
-      expect(fibRec(20)).toEqual(6765);
-  });
-});
-
-describe.skip('fibDyn', () => {
-  it('given place, retursn the Fibonacci number', () => {
-     expect(fibDyn(10)).toEqual(55);
-      expect(fibDyn(1)).toEqual(1);
-      expect(fibDyn(0)).toEqual(0);
-      expect(fibDyn(20)).toEqual(6765);
-  });
-});
-
-describe.skip('fibIter', () => {
-  it('given place, retursn the Fibonacci number', () => {
-     expect(fibIter(10)).toEqual(55);
+describe('fibIter', () => {
+  it('given place, returns the Fibonacci number', () => {
       expect(fibIter(1)).toEqual(1);
       expect(fibIter(0)).toEqual(0);
+      expect(fibIter(10)).toEqual(55);
       expect(fibIter(20)).toEqual(6765);
   });
 });
 
-describe.skip('toStr', () => {
+describe('fibRec', () => {
+  it('given place, returns the Fibonacci number', () => {
+      expect(fibRec(1)).toEqual(1);
+      expect(fibRec(0)).toEqual(0);
+      expect(fibRec(10)).toEqual(55);
+      expect(fibRec(20)).toEqual(6765);
+  });
+});
+
+describe('toStr', () => {
   it('given number and base, converts number to string of that base', () => {
     expect(toStr(199, 10)).toEqual('199');
-    expect(toStr(30, 1)).toEqual('11110');
+    expect(toStr(30, 2)).toEqual('11110');
   });
 });
